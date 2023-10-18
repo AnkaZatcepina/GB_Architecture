d6043b78ee02053fb256976f8b8ddc56c59ed8ad
anka <anka.zatcepina@gmail.com>
2023-10-18 18:03:18 +0300

    Lesson 2 Classwork Patterns

---
+++ lesson2/src/main/java/com/anna/lesson2/factorymethod/OperationSystemLogEventReader.java
@@ -0,0 +1,27 @@
+package com.anna.lesson2.factorymethod;
+
+import com.anna.lesson2.templatemethod.LogEntry;
+import com.anna.lesson2.templatemethod.LogReader;
+
+public class OperationSystemLogEventReader extends LogReader {
+    @Override
+    public Object getDataSource() {
+        return null;
+    }
+
+    @Override
+    public void setDataSource(Object data) {
+
+    }
+
+    @Override
+    protected Iterable<String> readEntries(int position) {
+        return null;
+    }
+
+    @Override
+    protected LogEntry parseLogEntry(String stringEntry) {
+        return null;
+    }
+}
+
